62f00f584b5c266c459cec42356013b5c09e221a
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-10 18:31:46 +1100

    [Feat] test, review 모듈 추가

--- models/index.js
+++ models/index.js
@@ -1,12 +1,14 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { Sequelize, DataTypes } = require('sequelize')
+const sequelize = require('../config/database')
 
-const User = require('./user')(sequelize);
-const Subject = require('./subject')(sequelize);
-const Classes = require('./classes')(sequelize);
-const Curriculum = require('./curriculum')(sequelize);
-const Chatroom = require('./chatroom')(sequelize);
-const Message = require('./message')(sequelize);
+const User = require('./user')(sequelize) 
+const Subject = require('./subject')(sequelize) 
+const Classes = require('./classes')(sequelize) 
+const Curriculum = require('./curriculum')(sequelize) 
+const Chatroom = require('./chatroom')(sequelize) 
+const Message = require('./message')(sequelize) 
+const Test = require('./test')(sequelize)
+const Review = require('./review')(sequelize)
 
 //모델 exports
 module.exports = {
@@ -17,4 +19,6 @@ module.exports = {
     Message,
     Chatroom,
     Message,
-};
+    Test,
+    Review
+} 
