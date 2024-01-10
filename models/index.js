d094a7d934460955e460da879c9a57e46d2b2b49
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-10 21:20:30 +1100

    [Feat] 모델 모듈 추가

--- models/index.js
+++ models/index.js
@@ -9,6 +9,9 @@ const Chatroom = require('./chatroom')(sequelize)
 const Message = require('./message')(sequelize) 
 const Test = require('./test')(sequelize)
 const Review = require('./review')(sequelize)
+const EnrolledClasses = require('./enrolled_classes')(sequelize)
+const WatchHistories = require('./watch_history')(sequelize)
+const Favorite = require('./favorite')(sequelize)
 
 //모델 exports
 module.exports = {
@@ -16,9 +19,11 @@ module.exports = {
     Subject,
     Classes,
     Curriculum,
-    Message,
     Chatroom,
     Message,
     Test,
-    Review
+    Review,
+    EnrolledClasses,
+    WatchHistories,
+    Favorite,
 } 
