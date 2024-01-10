e7a16a022fb729c3ce0535cf435c01d28cbe1859
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-10 22:56:48 +1100

    [Feat] 라우터 추가

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const port = 3000
 
 // routes
 const userRouter = require('./routes/user') // Adjust the path as needed
-app.get('/', (req, res) => {
-    res.send('Do Learn! Do Run!')
-})
+const classRouter = require('./routes/classes')
+const curriculumRouter = require('./routes/curriculum')
+const reviewRouter = require('./routes/review')
+const authRouter = require('./routes/auth')
 
-const classRouter=require('./routes/classes')
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Do Learn! Do Run!')
 })
 
@@ -19,7 +19,11 @@ app.use(express.json())
 
 // router
 app.use('/users', userRouter)
-app.use('/classes',classRouter)
+app.use('/classes', classRouter)
+app.use('/curriculums', curriculumRouter)
+app.use('/reviews', reviewRouter)
+app.use('/auth', authRouter)
+
 
 // sequalize
 sequelize.sync()
@@ -29,6 +33,11 @@ sequelize.sync()
     .catch((err) => {
         console.error('Error syncing database:', err)
     });
+
+
+app.get('/', (req, res) => {
+    res.send('Do Learn! Do Run!')
+})
 
 
 app.listen(port, () => {
