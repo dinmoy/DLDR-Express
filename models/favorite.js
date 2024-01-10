004e3a1f0bdd73b2c9cee5496d200cadec326f60
dinmoy <imhyojin614@gmail.com>
2024-01-10 18:23:28 +1100

    [Feat] favorite model

---
+++ models/favorite.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize')
+
+const Favorite = (sequelize) => {
+    return sequelize.define('favorites', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+        class_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'classes',
+                key: 'id',
+            },
+        },
+        is_deleted: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    });
+};
+
+module.exports = Favorite;
