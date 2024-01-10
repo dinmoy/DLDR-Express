9b4421d71f64fc9c35cfa6dd0893cdfd78bc32f8
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-10 18:15:48 +1100

    [Feat] 인덱스 모듈 추가

--- models/index.js
+++ models/index.js
@@ -4,7 +4,9 @@ const sequelize = require('../config/database');
 const User = require('./user')(sequelize);
 const Subject = require('./subject')(sequelize);
 const Classes = require('./classes')(sequelize);
-const Curriculum =require('./curriculum')(sequelize);
+const Curriculum = require('./curriculum')(sequelize);
+const Chatroom = require('./chatroom')(sequelize);
+const Message = require('./message')(sequelize);
 
 //모델 exports
 module.exports = {
@@ -12,4 +14,7 @@ module.exports = {
     Subject,
     Classes,
     Curriculum,
+    Message,
+    Chatroom,
+    Message,
 };
