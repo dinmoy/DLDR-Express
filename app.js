16b4b4690db1d0e33e82e39c14f3a49d1438a315
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-10 15:09:54 +1100

    [Feat] users 라우터 연결 및 시퀄라이즈 싱크

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const sequelize = require('./config/database')
 const app = express()
 const port = 3000
 
+// routes
+const userRouter = require('./routes/user') // Adjust the path as needed
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Do Learn! Do Run!')
 })
+
+// middlewares
+app.use(express.json())
+
+// router
+app.use('/users', userRouter)
+
+// sequalize
+sequelize.sync()
+    .then(() => {
+        console.log('Database synced')
+    })
+    .catch((err) => {
+        console.error('Error syncing database:', err)
+    });
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
