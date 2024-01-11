c4029097878b4db68c2afb83eadf77547535c21b
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-11 11:03:28 +1100

    [Feat] read user's classes endpoint

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { User } = require('../models')// user model
+const {User, Classes} = require('../models')// user model
 
 const router = express.Router();
 
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         const uploadDir = path.join(__dirname, '../uploads/profiles');
         if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir, { recursive: true });
+            fs.mkdirSync(uploadDir, {recursive: true});
         }
         cb(null, uploadDir);
     },
@@ -21,13 +21,13 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage });
+const upload = multer({storage});
 
 // 프로필 이미지 업로드 라우트
 router.post('/upload', upload.single('profileImage'), async (req, res) => {
     try {
         // 업로드된 파일 정보
-        const { filename, path: filePath } = req.file;
+        const {filename, path: filePath} = req.file;
 
         // 사용자 모델 업데이트
         const userId = req.body.userId; // 이 부분은 실제 사용자 ID를 전달해야 합니다.
@@ -35,13 +35,17 @@ router.post('/upload', upload.single('profileImage'), async (req, res) => {
         if (user) {
             user.profile_img = path.relative(path.join(__dirname, '..'), filePath);
             await user.save();
-            res.status(200).json({ success: true, message: 'Profile image uploaded successfully', profile_img: user.profile_img });
+            res.status(200).json({
+                success: true,
+                message: 'Profile image uploaded successfully',
+                profile_img: user.profile_img
+            });
         } else {
-            res.status(404).json({ success: false, message: 'User not found' });
+            res.status(404).json({success: false, message: 'User not found'});
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ success: false, message: 'Error uploading profile image' });
+        res.status(500).json({success: false, message: 'Error uploading profile image'});
     }
 });
 
@@ -51,7 +55,7 @@ router.get('/', async (req, res) => {
         const users = await User.findAll();
         return res.status(200).json(users);
     } catch (error) {
-        return res.status(500).json({ error: 'Error fetching users' });
+        return res.status(500).json({error: 'Error fetching users'});
     }
 });
 
@@ -62,13 +66,28 @@ router.get('/:id', async (req, res) => {
     try {
         const user = await User.findByPk(userId);
         if (!user) {
-            return res.status(404).json({ error: 'User not found' });
+            return res.status(404).json({error: 'User not found'});
         }
         return res.status(200).json(user);
     } catch (error) {
-        return res.status(500).json({ error: 'Error fetching user' });
+        return res.status(500).json({error: 'Error fetching user'});
     }
 });
+
+// 유저의 클래스 조회하기
+router.get('/:id/classes', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        // TODO: 유저가 학생일 때
+
+        // 유저가 선생님일 때
+        const classes = await Classes.findAll({where: {user_id: userId}})
+        return res.status(200).json(classes);
+    } catch (error) {
+        return res.status(500).json({error: 'Error finding classes'})
+    }
+})
 
 // 유저 업데이트 하기
 router.put('/:id', async (req, res) => {
@@ -77,13 +96,13 @@ router.put('/:id', async (req, res) => {
     try {
         const user = await User.findByPk(userId);
         if (!user) {
-            return res.status(404).json({ error: 'User not found' });
+            return res.status(404).json({error: 'User not found'});
         }
 
         await user.update(req.body);
         return res.status(200).json(user);
     } catch (error) {
-        return res.status(500).json({ error: 'Error updating user' });
+        return res.status(500).json({error: 'Error updating user'});
     }
 });
 
@@ -94,13 +113,13 @@ router.delete('/:id', async (req, res) => {
     try {
         const user = await User.findByPk(userId);
         if (!user) {
-            return res.status(404).json({ error: 'User not found' });
+            return res.status(404).json({error: 'User not found'});
         }
 
         await user.destroy();
         return res.status(204).json();
     } catch (error) {
-        return res.status(500).json({ error: 'Error deleting user' });
+        return res.status(500).json({error: 'Error deleting user'});
     }
 });
 
