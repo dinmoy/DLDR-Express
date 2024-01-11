3b4e270edaf1945a0aeb78245bd2a62023c66253
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-11 11:25:06 +1100

    [Feat] create chatroom endpoint 이미 존재하는 채팅방은 추가하지 못함

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -6,6 +6,15 @@ const router = express.Router();
 // 새로운 채팅방 만들기
 router.post('/', async (req, res) => {
     try {
+
+        const existChatroom = await Chatroom.findOne({
+            where: req.body
+        })
+
+        if (existChatroom) {
+            return res.status(400).json({ error: 'ChatRoom is already existing'})
+        }
+
         const newChatroom = await Chatroom.create(req.body)
         return res.status(201).json(newChatroom)
     } catch (error) {
