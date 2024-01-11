1564e07489b88106ac3a928171849b91e27b7bc5
hyeg0121 <w2218@e-mirim.hs.kr>
2024-01-11 11:02:04 +1100

    [Feat] update favorite endpoint

--- routes/favorite.js
+++ routes/favorite.js
@@ -13,8 +13,36 @@ router.post('/', async (req, res) => {
         return res.status(201).json(newFavorite);
     } catch (error) {
         console.log(error)
-        return res.status(500).json({ error: "Error creating favorite"})
+        return res.status(500).json({ error: 'Error creating favorite'})
     }
 })
 
+router.put('/:id', async (req, res) => {
+    try {
+        const favoriteId = req.params.id
+        const { user_id, class_id } = req.body
+
+        const updatedFavorite = await Favorite.findOne({
+            where: {
+                user_id,
+                class_id
+            }
+        })
+
+        if (!updatedFavorite) {
+            return res.status(404).json({ error: 'Cannot find favorite'})
+        }
+
+        const isDeleted = updatedFavorite.is_deleted === 1 ? 0 : 1
+
+        await updatedFavorite.update({
+            ...updatedFavorite,
+            is_deleted: isDeleted
+        })
+
+        return res.status(200).json(updatedFavorite);
+    } catch (error) {
+        return res.status(500).json({ error: 'Error updating favorites'})
+    }
+})
 module.exports = router;
