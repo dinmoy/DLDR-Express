69620fd2f06cf53e2693e14d1adf1b06d7ad69ff
dinmoy <imhyojin614@gmail.com>
2024-01-15 13:31:10 +1100

    [Feat] upload video

--- routes/classes.js
+++ routes/classes.js
@@ -1,57 +1,8 @@
 const { Op } = require('sequelize')
 const express = require('express')
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
 const { Classes, User, Review, Curriculum, Chatroom, Favorite } = require('../models')
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadDir = path.join(__dirname, '../uploads/video');
-        if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir, { recursive: true });
-        }
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const fileName = `${Date.now()}_${file.originalname}`;
-        cb(null, fileName);
-    },
-});
-
-const upload = multer({ storage });
-
-// upload video
-router.post('/upload', upload.single('videofile'), async (req, res) => {
-    try {
-        const { filename, path: filePath } = req.file;
-
-        const classId = req.body.classId;
-        const classes = await Classes.findByPk(classId);
-        if (classes) {
-            classes.videofile = path.relative(path.join(__dirname, '..'), filePath);
-            await classes.save();
-            res.status(200).json({
-                success: true,
-                message: 'Video uploaded successfully',
-                videofile: classes.videofile
-            });
-        } else {
-            res.status(404).json({
-                success: false,
-                message: 'Class not found'
-            });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: 'Error uploading video'
-        });
-    }
-});
 
 // read all classes
 router.get('/', async (req, res) => {
