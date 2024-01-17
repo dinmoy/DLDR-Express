28c3c01ac0cc4648338e962a0f38b1c73c2beaec
dinmoy <imhyojin614@gmail.com>
2024-01-17 15:55:08 +1100

    [Feat] update class

--- routes/classes.js
+++ routes/classes.js
@@ -1,10 +1,12 @@
-const {Op} = require('sequelize')
+const { Op } = require('sequelize')
 const sequelize = require('sequelize')
 const express = require('express')
-const {Classes, User, Review, Curriculum, Chatroom, Favorite} = require('../models')
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const { Classes, User, Review, Curriculum, Chatroom, Favorite, WatchHistories } = require('../models');
+const { createCipheriv } = require('crypto');
+
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -51,6 +53,7 @@ router.post('/upload', upload.single('thumbnail'), async (req, res) => {
         });
     }
 });
+
 
 // read all classes
 router.get('/', async (req, res) => {
@@ -110,76 +113,66 @@ router.get('/', async (req, res) => {
         } else {
             classes = await Classes.findAll();
         }
-
-        const userPromises = classes.map(async oneClass => {
-            return await User.findOne({
-                where: {
-                    id: oneClass.user_id
-                }
-            })
-        });
-
-        const users = await Promise.all(userPromises);
-
-        console.log('users', users);
-        const data = classes.map((oneClass, index) => {
-            return {
-                ...oneClass.dataValues,
-                teacher: {
-                    ...users[index].dataValues
-                }
-            }
-        });
-
-        return res.status(200).json(data);
+        return res.status(200).json(classes);
     } catch (error) {
         console.log(error);
-        return res.status(500).json({error: 'Error reading classes'});
+        return res.status(500).json({ error: 'Error reading classes' });
     }
 });
 
-router.post('/', async (req, res) => {
-    try {
-        const newClass = await Classes.create(req.body);
-        return res.status(201).json(newClass);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: 'Error creating class' });
-    }
-});
 
 //read one class's all reviews
 router.get('/:id/reviews', async (req, res) => {
     const classId = req.params.id;
     try {
-        const classReviews = await Review.findAll({where: {class_id: classId}});
+        const classReviews = await Review.findAll({ where: { class_id: classId } });
         return res.status(200).json(classReviews);
     } catch (error) {
         console.error(error);
-        return res.status(500).json({error: 'Error reading reviews for the class'});
+        return res.status(500).json({ error: 'Error reading reviews for the class' });
     }
 });
 
+//isWated X
 //read one class's all curriculums
 router.get('/:id/curriculums', async (req, res) => {
     const classId = req.params.id;
+    const userId = req.body.userId; 
     try {
-        const classCurriculums = await Curriculum.findAll({where: {class_id: classId}});
+        const classCurriculums = await Curriculum.findAll({ where: { class_id: classId } });
+        if (userId) {
+            const watchedPromise = classCurriculums.map(async watchedClass => {
+                const watched = await WatchHistories.findOne({
+                    where: {
+                        curriculum_id: watchedClass.id, 
+                        user_id:userId
+                    }
+                });
+                return {
+                    ...watchedClass.dataValues,
+                    isWatched: watched?true:false,
+                };
+            });
+            const curriculumWithWatchStatus = await Promise.all(watchedPromise);
+            return res.status(200).json(curriculumWithWatchStatus);
+        }
         return res.status(200).json(classCurriculums);
     } catch (error) {
         console.log(error);
-        return res.status(500).json({error: 'Error reading class all curriculums'});
+        return res.status(500).json({ error: 'Error reading class all curriculums' });
     }
 });
+
+
 
 //read one class's all chatrooms
 router.get('/:id/chatrooms', async (req, res) => {
     const classId = req.params.id;
     try {
-        const classChatrooms = await Chatroom.findAll({where: {class_id: classId}});
+        const classChatrooms = await Chatroom.findAll({ where: { class_id: classId } });
         return res.status(200).json(classChatrooms);
     } catch (error) {
-        return res.status(500).json({error: 'Error reading chatrooms'})
+        return res.status(500).json({ error: 'Error reading chatrooms' })
     }
 });
 
@@ -187,12 +180,25 @@ router.get('/:id/chatrooms', async (req, res) => {
 router.get('/:id/favorite', async (req, res) => {
     const classId = req.params.id;
     try {
-        const classFavorite = await Favorite.findAll({where: {class_id: classId}});
+        const classFavorite = await Favorite.findAll({ where: { class_id: classId } });
         return res.status(200).json(classFavorite);
     } catch (error) {
-        return res.status(500).json({error: 'Error reading favorites'});
+        return res.status(500).json({ error: 'Error reading favorites' });
     }
 });
 
+router.put('/:id',async (req,res)=>{
+    const classId=req.params.id;
+    try{
+        const classes=await Classes.findByPk(classId);
+        if(!classes){
+            return res.status(404).json({error: 'Class not found'});
+        }
+        await classes.update(req.body);
+        return res.status(200).json(classes);
+    }catch(error){
+        return res.status(500).json({error:'Error updating class'});
+    }
+})
 
 module.exports = router;
