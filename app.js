67d5174f040b0732ac6d97358ba9a60f0dacb22d
dinmoy <imhyojin614@gmail.com>
2024-04-17 02:15:31 +0900

    fix: 불필요한 코드 제거

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const path = require('path')
 const http=require('http')
 const app = express()
 const server=http.createServer(app)
-const socketIO=require('socket.io')
-const io=socketIO(server)
 const port = 3000
 
 // 세션
@@ -75,21 +73,9 @@ app.get('/', (req, res) => {
     res.send('Do Learn! Do Run!')
 })
 
-io.on('connection',(socket)=>{
-    socket.on('chatting',(data)=>{
-        console.log(data)
-        io.to(data.receiverId).emit('chatting', data);
-    })
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-    
-})
 
 server.listen(port,()=> console.log(`server is running ${port}`))
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port} `)
-// })
+
 
 
 
