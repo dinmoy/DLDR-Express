2042b9c6c77c3750cd0ae37af0b1a60237818cb6
dinmoy <imhyojin614@gmail.com>
2024-04-20 23:28:50 +0900

    feat: socket 메시지 전송 메서드 추가

--- sockets/socket.js
+++ sockets/socket.js
@@ -5,8 +5,37 @@ const Socket=(server)=>{
     const io=socketIO(server)
     io.on('connection',(socket)=>{
         console.log('a user connected')
+        message(socket)
+        disconnect(socket)
     })
     return io
 }
 
+//메시지 전송
+const message = async (socket) => {
+    socket.on('reqMessage', async (messageObj) => {
+        try {
+            const { user_id, chatroom_id, message } = messageObj;
+            const newMessage = await Message.create({
+                user_id,
+                chatroom_id,
+                message,
+            })
+
+            await ChatRoom.update(
+                { last_chat: newMessage },
+                { where: { id: chatroom_id } }
+            )
+            socket.to(chatroom_id).emit('resMessage', newMessage);
+        } catch (error) {
+            console.log('Error sending message: ', error);
+        }
+    })
+}
+
+const disconnect=(socket)=>{
+    socket.on('disconnect',()=>{
+        console.log('socket disconnected')
+    })
+}
 module.exports=Socket
