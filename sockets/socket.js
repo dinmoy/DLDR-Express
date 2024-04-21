ddf241f30369732b2fe8ac7334b0daf600e7c1ed
dinmoy <imhyojin614@gmail.com>
2024-04-21 14:31:27 +0900

    feat: socket 특정 유저 채팅방 목록 조회 메서드 추가

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,17 +1,20 @@
-const socketIO=require('socket.io')
-const {User,ChatRoom,Message}=require('../models')
+const socketIO = require('socket.io');
+const { User, Chatroom, Message,Classes} = require('../models');
 
-const Socket=(server)=>{
-    const io=socketIO(server)
-    io.on('connection',(socket)=>{
-        console.log('a user connected')
-        message(socket)
-        disconnect(socket)
-    })
-    return io
-}
+const Socket = (server) => {
+    const io = socketIO(server);
 
-//메시지 전송
+    io.on('connection', (socket) => {
+        console.log('A user connected');
+        message(socket);
+        readChatRoomList(socket);
+        disconnect(socket);
+    });
+
+    return io;
+};
+
+// 메시지 전송
 const message = async (socket) => {
     socket.on('reqMessage', async (messageObj) => {
         try {
@@ -20,22 +23,54 @@ const message = async (socket) => {
                 user_id,
                 chatroom_id,
                 message,
-            })
+            });
 
             await ChatRoom.update(
                 { last_chat: newMessage },
                 { where: { id: chatroom_id } }
-            )
+            );
+
             socket.to(chatroom_id).emit('resMessage', newMessage);
         } catch (error) {
             console.log('Error sending message: ', error);
         }
+    });
+};
+
+//특정 유저의 채팅방 목록 조회
+const readChatRoomList = (socket) => {
+    socket.on('reqChatRoomList', async (req) => {
+        try {
+            const { userId } = req;
+            const user = await User.findByPk(userId)
+            if (!user) {
+                socket.emit('loginError', { message: 'User not found' })
+            }
+            const rooms = await Chatroom.findAll({
+                where: { teacher_user_id: userId },
+            })
+
+            const chatroomDetails = await Promise.all(rooms.map(async (room) => {
+                const teacher = await User.findByPk(room.teacher_user_id);
+                const chatroomClass = await Classes.findByPk(room.class_id);
+                
+                return {
+                    ...room.dataValues, 
+                    teacher: teacher ? teacher.dataValues : null,
+                    class: chatroomClass ? chatroomClass.dataValues : null,
+                }
+            }))
+            socket.emit('resChatRoomList', chatroomDetails);
+        } catch (error) {
+            console.error('Error handling login:', error);
+        }
     })
 }
 
-const disconnect=(socket)=>{
-    socket.on('disconnect',()=>{
-        console.log('socket disconnected')
+const disconnect = (socket) => {
+    socket.on('disconnect', () => {
+        console.log('Socket disconnected')
     })
 }
-module.exports=Socket
+
+module.exports = Socket
